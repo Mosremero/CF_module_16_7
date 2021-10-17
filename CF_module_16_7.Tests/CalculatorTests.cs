cd4484af729928994857e06d4bc9d4a6f9f090cf
Zakharov Ivan <zakharov_ii@proitr.ru>
2021-10-17 21:01:04 +0300

    Add project files.

---
+++ CF_module_16_7.Tests/CalculatorTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace CF_module_16_7.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Additional_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Additional(5,10) == 15);
+        }
+        [Test]
+        public void Substraction_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Subtraction(50, 10) == 40);
+        }
+        [Test]
+        public void Miltiplication_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Miltiplication(10, 6) == 60);
+        }
+        [Test]
+        public void Division_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Division(10, 2) == 5);
+        }
+        [Test]
+        public void Division_MustThrowException()
+        {
+            var calculator = new Calculator();
+            Assert.Throws < DivideByZeroException > (() => calculator.Division(30, 0));
+        }
+    }
+}
---
+++ CF_module_16_7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CF_module_16_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
